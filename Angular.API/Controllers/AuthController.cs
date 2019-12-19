c85a889442d8e282ba17d3b41f244b0bc49d37e1
jaimeguardiano <jaimeguardiano199120@gmail.com>
2019-12-19 17:50:07 +1100

    last commit

---
+++ Angular.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using Angular.API.Data;
+using Angular.API.DTO;
+using Angular.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            return StatusCode(201);
+        }
+
+    }
+}
