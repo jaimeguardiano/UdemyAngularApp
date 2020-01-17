371bbb3f07f6626793e802111c26a75c3abf0238
jaimeguardiano <jaimeguardiano199120@gmail.com>
2020-01-17 17:12:46 +1100

    Error handling in API and Angular

--- AngularApp-SPA/src/app/nav/nav.component.ts
+++ AngularApp-SPA/src/app/nav/nav.component.ts
@@ -1,36 +1,40 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../_services/auth.service";
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
-  selector: "app-nav",
-  templateUrl: "./nav.component.html",
-  styleUrls: ["./nav.component.css"]
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-        console.log("Logged in successfully");
+        this.alertify.success('Logged in successfully');
       },
       error => {
-        console.log("Failed to login");
+        this.alertify.error('Failed to login');
       }
     );
   }
 
   loggedIn() {
-    const token = localStorage.getItem("token");
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   loggedOut() {
-    localStorage.removeItem("token");
-    console.log("logged out");
+    localStorage.removeItem('token');
+    this.alertify.message('logged out');
+    this.loggedIn();
   }
 }
